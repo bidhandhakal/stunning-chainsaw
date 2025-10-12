3cfd24161cf5c976952b432e7c9d83fbed621453
BIDHAN DHAKAL <bidhandhakal.web@gmail.com>
2025-10-12 14:55:09 +0545

    (-)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 import Image from "next/image";
 import DecayCard from "@/components/DecayCard";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import gsap from "gsap";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-
-    
-   <div className="flex items-start justify-end h-screen p-40 bg-black">
-    <DecayCard width={200} height={300}>
-    </DecayCard>
-   </div>
-   </div>
+    <div className="h-screen bg-[#0A0A0A]">
+      <div className="absolute top-0 left-0 p-15 flex items-center text-white">
+        <Avatar>
+          <AvatarImage src="https://github.com/bidhandhakal.png" />
+          <AvatarFallback>BD</AvatarFallback>
+        </Avatar>
+        <Button variant="link" className="text-white text-2xl" asChild>
+          <a
+            href="https://github.com/bidhandhakal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @bidhandhakal
+          </a>
+        </Button>
+        <Link href="/home">Go to </Link>
+      </div>
+      <div className="flex items-center justify-end p-40 h-screen bg-[#0A0A0A]">
+        <DecayCard width={250} height={300}></DecayCard>
+      </div>
+    </div>
   );
 }
