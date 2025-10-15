a6eb2dd0f271332131c8950006d618c169b1bfa0
BIDHAN DHAKAL <bidhandhakal.web@gmail.com>
2025-10-15 20:26:41 +0545

    added scroll reveal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Highlighter } from "@/components/ui/highlighter";
+import ScrollReveal from "@/components/ScrollReveal";
 
 export default function Home() {
   return (
     <main>
       <section className="min-h-screen flex items-center justify-center font-[font4] bg-[#F8F8ff]">
         <div className="text-center">
-          <p className="leading-relaxed text-7xl">
+          <p className="leading-relaxed text-3xl md:text-7xl">
             I'm{" "}
             <Highlighter action="underline" color="#FF9800">
               Bidhan
@@ -19,11 +20,31 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className="min-h-screen flex justify-center items-center bg-[#F8F8ff] font-[font6]">
-        <h2 className="text-6xl text-red-600">TWO</h2>
+      <section className="min-h-screen flex justify-center items-center bg-[#F8F8ff] font-[font4]">
+        <div className=" text-start p-15 md:p-50 ">
+          <ScrollReveal
+            baseOpacity={5}
+            enableBlur={true}
+            baseRotation={5}
+            blurStrength={10}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At ad eius
+            est vel magnam esse laudantium earum repellendus reiciendis ipsa?!
+          </ScrollReveal>
+        </div>
       </section>
-      <section className="min-h-screen flex justify-center items-center bg-[#F8F8ff] font-[font6]">
-        <h2 className="text-6xl text-blue-600">3</h2>
+      <section className="min-h-screen flex bg-[#F8F8ff] font-[font4]">
+        <div className=" text-start p-15 md:p-50">
+          <ScrollReveal
+            baseOpacity={5}
+            enableBlur={true}
+            baseRotation={5}
+            blurStrength={10}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At ad eius
+            est vel magnam esse laudantium earum repellendus reiciendis ipsa?!
+          </ScrollReveal>
+        </div>
       </section>
     </main>
   );
